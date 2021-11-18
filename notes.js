92328dfef8d459e72fbd1e47affebb8022306872
Henry Doan <henrydoan96@gmail.com>
2021-11-17 19:33:08 -0700

    added notes on var and transfer

---
+++ notes.js
@@ -0,0 +1,44 @@
+// Datatypes 
+// string "" ''
+// Boolean true false 
+// integer 12312 -123123 0 
+// float 234.234234
+// hash 
+// { first_name: "bob", age: 2 }
+// arr 
+// ['', 324234, []]
+// null 
+// NAN 
+// undefined 
+// + -  / * %
+// ()
+// || && 
+// < > <= >= 
+// =
+// ==
+// ===
+// !=
+// !==
+// ++
+// --
+// +=
+// -=
+// *=
+
+// variables
+// var firstName = 'bob'
+// var first_name = 'bob'
+// var class = 'bob'
+// var true = 'bob'
+// - letters, numbers, _ $ 
+// - has to begin with letter $ _
+// - case sensitive 
+// - stay away from reserve words 
+// - camelcase
+// var FirstName = 'bob'
+// no longer using var becuase of scope
+// var file wide scope 
+// use these instead
+// let - things you want to change
+// const - constant or things you don't want change
+// scope is where ever it is you define it in.
